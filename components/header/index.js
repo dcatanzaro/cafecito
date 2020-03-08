6c313ca8c7e16b013ced15a54249e1b4a7914f6b
Franco Cuarterolo <fcuarterolo@gmail.com>
2020-03-08 14:09:14 -0300

    refactor(Header): replace class with functional

--- components/header/index.js
+++ components/header/index.js
@@ -3,21 +3,17 @@ import style from "./style.scss";
 
 import { Follow } from "react-twitter-widgets";
 
-class Header extends React.Component {
-    render() {
-        return (
-            <header className={style.headerContainer}>
-                <div className={style.header}>
-                    <div className={style.profileImg}></div>
-                    <div className={style.name}>@DamianCatanzaro</div>
-                </div>
+const Header = () => (
+  <header className={style.headerContainer}>
+    <div className={style.header}>
+      <div className={style.profileImg}></div>
+      <div className={style.name}>@DamianCatanzaro</div>
+    </div>
 
-                <div className={style.twitter}>
-                    <Follow username="DamianCatanzaro" />
-                </div>
-            </header>
-        );
-    }
-}
+    <div className={style.twitter}>
+      <Follow username="DamianCatanzaro" />
+    </div>
+  </header>
+);
 
 export default Header;
