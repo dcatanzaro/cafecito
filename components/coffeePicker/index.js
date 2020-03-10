40099f598bf260296737d437dbec759c7b9484ab
matias4205 <matiasperezpc@hotmail.com>
2020-03-10 20:31:16 -0300

    Auto format changes

--- components/coffeePicker/index.js
+++ components/coffeePicker/index.js
@@ -1,27 +1,33 @@
-import React from 'react';
+import React from "react";
+import PropTypes from "prop-types";
 
-import styles from './style.scss';
+import styles from "./style.scss";
 
 const CoffeePicker = ({ countCoffees, ...props }) => {
     const { setCount } = props;
-    
-    const add = () => setCount(countCoffees + 1)
 
-    const subtract = () => setCount(countCoffees - 1)
+    const add = () => setCount(countCoffees + 1);
+
+    const subtract = () => setCount(countCoffees - 1);
 
     return (
         <div className={styles.controlsContainer}>
             <button className={styles.leftControl} onClick={subtract}>
-                <img src="/imgs/minus-icon.svg" alt=""/>
+                <img src="/imgs/minus-icon.svg" alt="" />
             </button>
 
-            <input value={ countCoffees } type="text"/>
-            
+            <input value={countCoffees} type="text" />
+
             <button className={styles.rightControl} onClick={add}>
-                <img src="/imgs/plus-icon.svg" alt=""/>
+                <img src="/imgs/plus-icon.svg" alt="" />
             </button>
         </div>
     );
+};
+
+CoffeePicker.propTypes = {
+    countCoffees: PropTypes.number,
+    setCount: PropTypes.func,
 };
 
 export default CoffeePicker;
--- pages/customCoffee/index.js
+++ pages/customCoffee/index.js
@@ -1,40 +1,39 @@
-import React, { Component } from 'react';
-import axios from 'axios';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import axios from "axios";
 
-import CoffePicker from '../../components/coffeePicker';
+import CoffePicker from "../../components/coffeePicker";
 
-import styles from './style.scss';
+import styles from "./style.scss";
 
 const COFFEE_PRICE = 50;
 
-
 /* Mini components */
 const ProfileImg = ({ imgSrc }) => (
-    <div className={styles.profileImg} >
-        <img src={imgSrc} alt="profile-img"/>
+    <div className={styles.profileImg}>
+        <img src={imgSrc} alt="profile-img" />
     </div>
 );
 
 const RedirectIcon = ({ url }) => (
     <a href={url} className={styles.redirect}>
-        <img src="/imgs/redirect-icon.svg" alt="redirect-icon"/>
+        <img src="/imgs/redirect-icon.svg" alt="redirect-icon" />
     </a>
 );
 
-
 class CustomCoffee extends Component {
     state = {
-        message: '',
-        name: '',
+        message: "",
+        name: "",
         countCoffees: 1,
-        loading: true
-    }
+        loading: true,
+    };
 
     sendCoffee = async () => {
         const { name, message, countCoffees } = this.state;
 
         this.setState({
-            loading: true
+            loading: true,
         });
 
         const url = `${process.env.URL}/api/send_coffee`;
@@ -42,56 +41,66 @@ class CustomCoffee extends Component {
         const result = await axios.post(url, {
             name,
             message,
-            countCoffees: countCoffees || 1
+            countCoffees: countCoffees || 1,
         });
 
         window.location.href = result.data.mercadoPagoLink;
     };
 
-    setCount = (value) => {
+    setCount = value => {
         this.setState({
-            countCoffees: value < 1 ? 1 : value
-        })
-    }
-
-    handleFormChange = (e) => {
-        this.setState({
-            name: e.target.value
+            countCoffees: value < 1 ? 1 : value,
         });
-    }
+    };
+
+    handleFormChange = e => {
+        this.setState({
+            name: e.target.value,
+        });
+    };
 
     render() {
         const { countCoffees, name } = this.state;
-        
+
         return (
             <div className={styles.main}>
                 <div className={styles.modalContainer}>
-                    <RedirectIcon  url="/" />
-                    
+                    <RedirectIcon url="/" />
+
                     <ProfileImg imgSrc="https://avatars2.githubusercontent.com/u/43894343?s=460&v=4" />
 
                     <h1 className={styles.title}>Gracias por escuchar</h1>
-                    <h3 className={styles.description}>Ayudame con un cafe ☕️!</h3>
+                    <h3 className={styles.description}>
+                        Ayudame con un cafe ☕️!
+                    </h3>
 
-                    <CoffePicker countCoffees={countCoffees} setCount={this.setCount} />
+                    <CoffePicker
+                        countCoffees={countCoffees}
+                        setCount={this.setCount}
+                    />
 
                     <input
-                        className={styles.input} 
-                        placeholder="Nombre o @Twitter (opcional)" 
+                        className={styles.input}
+                        placeholder="Nombre o @Twitter (opcional)"
                         value={name}
                         onChange={this.handleFormChange}
                         type="text"
                     />
-                    <button 
-                        className={styles.submit}
-                        onClick={this.sendCoffee}
-                    >
-                        Invitame 1 café (${ countCoffees * COFFEE_PRICE })
+                    <button className={styles.submit} onClick={this.sendCoffee}>
+                        Invitame 1 café (${countCoffees * COFFEE_PRICE})
                     </button>
                 </div>
             </div>
         );
     }
 }
+
+ProfileImg.propTypes = {
+    imgSrc: PropTypes.string,
+};
+
+RedirectIcon.propTypes = {
+    url: PropTypes.string,
+};
 
 export default CustomCoffee;
