c17b1ecc9bc3db0d0e1d46e9b131a074d74217be
Damián <damiancatanzaro@gmail.com>
2020-03-10 19:10:49 -0300

    next-css

--- next.config.js
+++ next.config.js
@@ -5,16 +5,20 @@ const envFile = isDev ? `.env.${process.env.NODE_ENV}` : ".env";
 dotenv.config({ path: envFile });
 
 const withSass = require("@zeit/next-sass");
+const withCSS = require("@zeit/next-css");
 
-module.exports = withSass({
-    env: {
-        BRANCH: process.env.BRANCH,
-        URL: process.env.URL,
-        SHOW_DATE_COFFEE: process.env.SHOW_DATE_COFFEE === "true" ? true : false
-    },
-    cssModules: true,
-    cssLoaderOptions: {
-        importLoaders: 1,
-        localIdentName: "[local]__[hash:base64:5]"
-    }
-});
+module.exports = withCSS(
+    withSass({
+        env: {
+            BRANCH: process.env.BRANCH,
+            URL: process.env.URL,
+            SHOW_DATE_COFFEE:
+                process.env.SHOW_DATE_COFFEE === "true" ? true : false,
+        },
+        cssModules: true,
+        cssLoaderOptions: {
+            importLoaders: 1,
+            localIdentName: "[local]__[hash:base64:5]",
+        },
+    })
+);
