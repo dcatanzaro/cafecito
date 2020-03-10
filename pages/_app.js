ebbc930418e2d66308a6c1568f9eaf067ee89fcb
Damián <damiancatanzaro@gmail.com>
2020-03-10 19:06:48 -0300

    autoAddCss to FontAwesome

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,11 @@ import React from "react";
 import PropTypes from "prop-types";
 import Head from "next/head";
 
+import "@fortawesome/fontawesome-svg-core/styles.css";
 import "../styles/style.scss";
+
+import { config as configFA } from "@fortawesome/fontawesome-svg-core";
+configFA.autoAddCss = false;
 
 function MyApp({ Component, pageProps }) {
     return (
