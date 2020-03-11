e8dc4766a390f283b0726b09c1a408f0f3740185
matias4205 <matiasperezpc@hotmail.com>
2020-03-11 20:26:01 -0300

    index on custom-coffee: 563304a Merge branch 'master' into custom-coffee

--- components/coffeePicker/index.js
+++ components/coffeePicker/index.js
@@ -13,13 +13,13 @@ const CoffeePicker = ({ countCoffees, ...props }) => {
     return (
         <div className={styles.controlsContainer}>
             <button className={styles.leftControl} onClick={subtract}>
-                <img src="/imgs/minus-icon.svg" alt="" />
+                -
             </button>
 
             <input value={countCoffees} type="text" />
 
             <button className={styles.rightControl} onClick={add}>
-                <img src="/imgs/plus-icon.svg" alt="" />
+                +
             </button>
         </div>
     );
